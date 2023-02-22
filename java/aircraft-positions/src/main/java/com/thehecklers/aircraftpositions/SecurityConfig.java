d99525c7fd7e5823c5d8a20b501fee6ab339bb78
Jung Dae Suh <jd.seo@handys.co.kr>
2023-02-23 00:57:49 +0900

    어노테이션 수정

--- java/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
+++ java/aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.thehecklers.aircraftpositions;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
     private final PasswordEncoder pwEncoder =
             PasswordEncoderFactories.createDelegatingPasswordEncoder();
